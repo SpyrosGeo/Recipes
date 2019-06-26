e4384ae9b07e30db051c39f9daa740bf49e5600b
thatguy <xarodasimba@gmail.com>
2019-06-26 17:42:23 +0300

    Added ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,21 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from "./recipe.model";
+import { Ingredient }from '../shared/ingredient.model';
 
 
 export class RecipeService {
 recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('First Recipe','This is a test','https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'),
-    new Recipe('A Second Recipe','This is a test','https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'),
-    new Recipe('A third Recipe','This is a test','https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80')
+    new Recipe('Cheesy Pork Sausage Flatbreads','Tasty Flatbreat','https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_400,q_auto/v1/hellofresh_s3/5cf80b721673620013382c95/step-c0819f7e.jpg',[
+
+      new Ingredient('Meat',1),
+      new Ingredient('Cheese',5)
+    ]),
+    new Recipe('Annie’s Organic Cinnamon Rolls','with Gooey Icing Drizzle (makes 5 cinnamon rolls)','https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,q_auto,w_2600/v1/hellofresh_s3/image/w27-annie-s-organic-cinnamon-rolls-36df5ba7.jpg',[
+      new Ingredient('milk',5),
+      new Ingredient('wheat',3)
+    ])
   ];
   getRecipes(){
     //we use slice with no arguments so we cant access the recipes at all
