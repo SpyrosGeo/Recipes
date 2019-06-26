04fb3675c01228db6c4b453cb5fd12391c689c76
thatguy <xarodasimba@gmail.com>
2019-06-26 16:58:55 +0300

    Improved Recipe section functionality using the service instead of Input,Output

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
+
 
 @Component({
   selector: 'app-recipe-item',
@@ -11,12 +13,13 @@ export class RecipeItemComponent implements OnInit {
   //fetched the recipe from recipe.model using the Input decorator
   @Input() recipe: Recipe;
   //created a new event emits data when the recipe item link is clicked
-  @Output() recipeSelected = new EventEmitter<void>();
-  constructor() { }
+
+  constructor(private recipeService:RecipeService) { }
 
   ngOnInit() {
   }
+
   onSelected(){
-    this.recipeSelected.emit();
+    this.recipeService.recipeSelected.emit(this.recipe);
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,10 @@
+import { EventEmitter } from '@angular/core';
+
 import { Recipe } from "./recipe.model";
 
 
 export class RecipeService {
-
+recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe('First Recipe','This is a test','https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'),
     new Recipe('A Second Recipe','This is a test','https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'),
