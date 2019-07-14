b14982482a6837966bc97b9e3d3b081f0fa604b2
thatguy <xarodasimba@gmail.com>
2019-07-14 20:35:36 +0300

    Created the tepmplate for the recipe Edit Form (reactive) and the created the form for  editing recipes Synced HTML with the Form and added Ingredient controls to a Form Array.Finally added new Ingredient Controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -32,7 +32,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
