836e325e0b36dd2e27cfeeae4035a074ba61f5e5
thatguy <xarodasimba@gmail.com>
2019-07-22 15:56:20 +0300

    added responce data transformation and resolving data before loading functionality

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,17 +8,18 @@ import { ShoppingListService }from '../shopping-list/shopping-list.service';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject <Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe('Cheesy Pork Sausage Flatbreads','Tasty Flatbreat','https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_400,q_auto/v1/hellofresh_s3/5cf80b721673620013382c95/step-c0819f7e.jpg',[
-
-      new Ingredient('Meat',1),
-      new Ingredient('Cheese',5)
-    ]),
-    new Recipe('Annie’s Organic Cinnamon Rolls','with Gooey Icing Drizzle (makes 5 cinnamon rolls)','https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,q_auto,w_2600/v1/hellofresh_s3/image/w27-annie-s-organic-cinnamon-rolls-36df5ba7.jpg',[
-      new Ingredient('milk',5),
-      new Ingredient('wheat',3)
-    ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('Cheesy Pork Sausage Flatbreads','Tasty Flatbreat','https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_400,q_auto/v1/hellofresh_s3/5cf80b721673620013382c95/step-c0819f7e.jpg',[
+  //
+  //     new Ingredient('Meat',1),
+  //     new Ingredient('Cheese',5)
+  //   ]),
+  //   new Recipe('Annie’s Organic Cinnamon Rolls','with Gooey Icing Drizzle (makes 5 cinnamon rolls)','https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,q_auto,w_2600/v1/hellofresh_s3/image/w27-annie-s-organic-cinnamon-rolls-36df5ba7.jpg',[
+  //     new Ingredient('milk',5),
+  //     new Ingredient('wheat',3)
+  //   ])
+  // ];
+  private recipes:Recipe[] = [];
   constructor(private slService:ShoppingListService){}
 
   setRecipes(recipes:Recipe[]){
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map,tap } from 'rxjs/operators';
 
 
 
@@ -21,8 +22,14 @@ export class DataStorageService {
 
 
   fetchRecipes(){
-    this.http.get<Recipe[]>('https://ng-course-recipe-book-7b64d.firebaseio.com/recipes.json').subscribe(recipes =>{
+  return  this.http.get<Recipe[]>('https://ng-course-recipe-book-7b64d.firebaseio.com/recipes.json').pipe(map(recipes=>{
+      return recipes.map(recipe=>{
+        return{...recipe,ingredients:recipe.ingredients ? recipe.ingredients : []}
+      })
+    }),
+    tap( recipes=>{
       this.recipeService.setRecipes(recipes)
     })
+  )
   }
 }
