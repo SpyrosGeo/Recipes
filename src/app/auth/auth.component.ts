ab5d89195e00a0d0b06c5d43ad891ee775ed8343
thatguy <xarodasimba@gmail.com>
2019-07-23 19:43:30 +0300

    sending login requests logic, login error handling and creating & storing user data

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
-
-import { AuthService } from './auth.service';
+import { AuthService,AuthResponseData } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -24,20 +24,24 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+
+
     this.isLoading = true ;
     if (this.isLoginMode) {
-      //....
+    authObs =  this.authService.login(email,password)
     }else {
-      this.authService.signUp(email,password).subscribe(resData =>{
-        console.log(resData);
-        this.isLoading = false;
-      },errorMessage=>{
-      console.log(errorMessage);
-      this.error = errorMessage;
-      this.isLoading = false;
-      });
+    authObs =  this.authService.signUp(email,password)
     }
-
+    authObs.subscribe(resData =>{
+      console.log(resData);
+      this.isLoading = false;
+    },errorMessage=>{
+    console.log(errorMessage);
+    this.error = errorMessage;
+    this.isLoading = false;
+    });
     form.reset();
   }
 
