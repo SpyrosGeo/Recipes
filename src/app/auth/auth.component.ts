6d638b0cbce84fb03adf45549bfaf9434fab0c96
thatguy <xarodasimba@gmail.com>
2019-07-23 20:07:36 +0300

    added auth state reflaction in the UI of the app Now only the Authenticate and Shopping list are available for viewing if u are not logged in

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
-
+import { Router } from '@angular/router';
 import { AuthService,AuthResponseData } from './auth.service';
 
 @Component({
@@ -13,7 +13,7 @@ export class AuthComponent {
   isLoginMode = true;
   isLoading = false;
   error:string  = null;
-  constructor(private authService:AuthService){}
+  constructor(private authService:AuthService, private router:Router){}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -34,9 +34,12 @@ export class AuthComponent {
     }else {
     authObs =  this.authService.signUp(email,password)
     }
+
+
     authObs.subscribe(resData =>{
       console.log(resData);
       this.isLoading = false;
+      this.router.navigate(['/recipes']);
     },errorMessage=>{
     console.log(errorMessage);
     this.error = errorMessage;
