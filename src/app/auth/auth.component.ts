16c9e25da2cbc14be82bbd04c877696777126a33
thatguy <xarodasimba@gmail.com>
2019-08-13 01:42:59 +0300

    some more cleanup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,8 @@
 import { Component, ComponentFactoryResolver, ViewChild, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Observable, Subscription } from 'rxjs';
-import { Router } from '@angular/router';
-import { AuthService, AuthResponseData } from './auth.service';
+import { Subscription } from 'rxjs';
+
+
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 import { Store } from '@ngrx/store';
@@ -24,8 +24,7 @@ export class AuthComponent implements OnInit, OnDestroy {
 
   private closeSub: Subscription;
   private storeSub: Subscription;
-  constructor(private authService: AuthService,
-    private router: Router,
+  constructor(
     private componentFactoryResolver: ComponentFactoryResolver,
     private store: Store<fromApp.AppState>) { }
 
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,20 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 
-
-
 import * as AuthActions from './store/auth.actions';
 import * as fromApp from '../store/app.reducer';
 
-export interface AuthResponseData {
-  kind: string;
-  idToken: string;
-  email: string;
-  refreshToken: string;
-  expiresIn: string;
-  localId: string;
-  registered?: boolean;
-}
+
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
