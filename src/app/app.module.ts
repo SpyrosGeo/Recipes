73ac2b733bb45fb07497d9d5bed7fc6312ed981b
thatguy <xarodasimba@gmail.com>
2019-08-13 02:25:25 +0300

    started refactoring code for recipes using ngrx. i created the recipe.action/reducer/effects files and implemented the SET_RECIPES action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule } from '@angular/common/http';
 // import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 // import { authReducer } from './auth/store/auth.reducer';
-
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import {StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { CoreModule } from './core.module';
@@ -12,9 +13,9 @@ import { AlertComponent } from './shared/alert/alert.component';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
-import { AuthEffects } from './auth/store/auth.effects';
 import * as fromApp from './store/app.reducer';
-
+import { AuthEffects } from './auth/store/auth.effects';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,6 +27,8 @@ import * as fromApp from './store/app.reducer';
     HttpClientModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly:environment.production}),
+    StoreRouterConnectingModule.forRoot(),
     SharedModule,
     CoreModule,
   ],
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router ,ActivatedRoute} from '@angular/router';
 import { Subscription } from 'rxjs';
-
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
+import * as fromApp from '../../store/app.reducer';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from "../recipe.service";
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -14,11 +17,19 @@ export class RecipeListComponent implements OnInit,OnDestroy {
   recipes: Recipe[];
   subscription:Subscription
 
-  constructor(private recipeService:RecipeService,private router:Router,private route: ActivatedRoute) { }
+  constructor(private recipeService:RecipeService,
+              private router:Router,
+              private route: ActivatedRoute,
+              private store:Store<fromApp.AppState>) { }
 
   ngOnInit() {
 
-    this.subscription = this.recipeService.recipesChanged.subscribe((recipes:Recipe[])=>{
+    this.subscription = this.store.select('recipes')
+    .pipe(map(recipesState =>{
+      return recipesState.recipes
+    })
+  )
+    .subscribe((recipes:Recipe[])=>{
       this.recipes = recipes;
     })
     this.recipes = this.recipeService.getRecipes();
